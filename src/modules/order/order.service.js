7cbf0351a672dc5efce310181d8b980cfb180275
Le Thanh Tra <lethanhtra06092004@gmail.com>
2026-06-11 19:46:10 +0700

    feat: implement customer order feature

--- src/modules/order/order.service.js
+++ src/modules/order/order.service.js
@@ -6,6 +6,7 @@ const create = (data) => Order.create(data);
 const list = async (query = {}) => {
   const { page, limit, skip } = getPagination(query);
   const filter = {};
+  if (query.userId) filter.userId = query.userId;
   if (query.isActive !== undefined) filter.isActive = query.isActive === "true";
   if (query.status) filter.status = query.status;
   if (query.type) filter.type = query.type;
@@ -18,7 +19,20 @@ const list = async (query = {}) => {
     ];
 
   const [items, total] = await Promise.all([
-    Order.find(filter).skip(skip).limit(limit).sort({ createdAt: -1 }),
+    Order.find(filter)
+      .populate("queue")
+      .populate({
+        path: "items",
+        populate: {
+          path: "menuScheduleItemId",
+          populate: {
+            path: "foodId",
+          },
+        },
+      })
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 }),
     Order.countDocuments(filter),
   ]);
 
@@ -28,7 +42,18 @@ const list = async (query = {}) => {
   };
 };
 
-const getById = (id) => Order.findById(id);
+const getById = (id) =>
+  Order.findById(id)
+    .populate("queue")
+    .populate({
+      path: "items",
+      populate: {
+        path: "menuScheduleItemId",
+        populate: {
+          path: "foodId",
+        },
+      },
+    });
 const updateById = (id, data) =>
   Order.findByIdAndUpdate(id, data, { new: true, runValidators: true });
 const deleteById = (id) => Order.findByIdAndDelete(id);
