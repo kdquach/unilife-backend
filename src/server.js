314ac01b010d76649b7e1775ca340bec6be570e1
trangkhoi <trangkhoi1512st@gmail.com>
2026-06-24 10:28:52 +0700

    feat(user-notification): add notification feature

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
+const http = require("http");
 const app = require("./app");
 const connectDB = require("./config/db.config");
+const { initSocket } = require("./socket");
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+initSocket(server);
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`UniLife API is running on port ${PORT}`);
   });
 });
