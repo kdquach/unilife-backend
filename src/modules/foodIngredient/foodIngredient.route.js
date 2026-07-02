3acd6d9726ea6459a4572dd8570aa5a5d48c5f14
kdquach <kd.quach03@gmail.com>
2026-07-02 10:00:02 +0700

    fix(food-ingredients): restrict access to kitchen roles

--- src/modules/foodIngredient/foodIngredient.route.js
+++ src/modules/foodIngredient/foodIngredient.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const controller = require("./foodIngredient.controller");
 const { authenticate } = require("../../middlewares/auth.middleware");
+const { authorize } = require("../../middlewares/role.middleware");
+const ROLES = require("../../constants/roles.constant");
 
 const router = express.Router();
 
 router.use(authenticate);
+router.use(authorize(ROLES.ADMIN, ROLES.MANAGER, ROLES.KITCHEN_STAFF));
 router.get("/", controller.list);
 router.post("/", controller.create);
 router.get("/:id", controller.getById);
