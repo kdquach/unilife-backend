af95e0a4512e5b875260fbdf3ca0c74bc63f9bb2
trangkhoi <trangkhoi1512st@gmail.com>
2026-07-04 17:55:19 +0700

    add View Ratings, Create Rating, Update Rating, Delete Rating for Customer

--- src/modules/rating/rating.route.js
+++ src/modules/rating/rating.route.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const controller = require("./rating.controller");
 const { authenticate } = require("../../middlewares/auth.middleware");
+const { authorize } = require("../../middlewares/role.middleware");
+const ROLES = require("../../constants/roles.constant");
 
 const router = express.Router();
 
-router.use(authenticate);
 router.get("/", controller.list);
-router.post("/", controller.create);
-router.get("/:id", controller.getById);
-router.patch("/:id", controller.updateById);
-router.delete("/:id", controller.deleteById);
+
+router.use(authenticate);
+router.get("/me", authorize(ROLES.CUSTOMER), controller.listMine);
+router.get("/me/:id", authorize(ROLES.CUSTOMER), controller.getMineById);
+router.post("/", authorize(ROLES.CUSTOMER), controller.create);
+router.patch("/:id", authorize(ROLES.CUSTOMER), controller.updateById);
+router.delete("/:id", authorize(ROLES.CUSTOMER), controller.deleteById);
+router.get("/:id", authorize(ROLES.ADMIN, ROLES.MANAGER), controller.getById);
 
 module.exports = router;
