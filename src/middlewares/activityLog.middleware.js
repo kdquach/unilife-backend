98f90e408d4b39d6efcdfdc1bed7179edec6b4f3
Le Thanh Tra <lethanhtra06092004@gmail.com>
2026-07-26 15:16:23 +0700

    feat: add authorization middleware to activity log route and add tests

--- src/middlewares/activityLog.middleware.js
+++ src/middlewares/activityLog.middleware.js
@@ -4,6 +4,8 @@ const generateDescription = (action, req) => {
   const body = req.body || {};
   
   switch (action) {
+    case "LOGIN":
+      return "Logged into the system";
     case "CREATE_USER":
       return `Created new user account: ${body.fullName || ""} (${body.email || ""}) with role ${body.role || ""}`;
     case "UPDATE_USER":
--- src/modules/activityLog/activityLog.route.js
+++ src/modules/activityLog/activityLog.route.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const controller = require("./activityLog.controller");
 const { authenticate } = require("../../middlewares/auth.middleware");
+const { authorize } = require("../../middlewares/role.middleware");
+const ROLES = require("../../constants/roles.constant");
 
 const router = express.Router();
 
 router.use(authenticate);
+router.use(authorize(ROLES.ADMIN, ROLES.MANAGER));
+
 router.get("/", controller.list);
 router.get("/stats", controller.getStats);
 router.post("/", controller.create);
